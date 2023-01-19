b76085664543c9e7aec56c3eacdc95d9e9955a7e
Tomek <tomasz.ficner@gazeta.pl>
2023-01-19 23:47:52 +0100

    first variables added

--- src/main/java/sweatBloodAndTears/WashingMachine.java
+++ src/main/java/sweatBloodAndTears/WashingMachine.java
@@ -2,6 +2,9 @@ package sweatBloodAndTears;
 
 public class WashingMachine {
 
+    int loathWeight; //max 9kg
+    int programType; //1 - standard 2 - fast 3 - eco
+
     void start() {
         System.out.println("Washing in progress");
     }
